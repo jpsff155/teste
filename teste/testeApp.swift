625e9d4bd2988e0aa42077e5eaaf5bb68dd865fb
joao <soaresjp1511@gmail.com>
2025-03-12 16:55:09 -0300

    Initial Commit

---
+++ teste/testeApp.swift
@@ -0,0 +1,17 @@
+//
+//  testeApp.swift
+//  teste
+//
+//  Created by found on 12/03/25.
+//
+
+import SwiftUI
+
+@main
+struct testeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
